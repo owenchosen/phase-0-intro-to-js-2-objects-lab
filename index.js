7a636d616aeefe34dfbae44d94eb133d7f6eaf88
owen <owen.chosen@gmail.com>
2023-07-28 23:26:42 +0100

    Done

--- index.js
+++ index.js
@@ -1,1 +1,27 @@
 // Write your solution in this file!
+const employee = {
+    name: "Onyechefu Owen",
+    streetAddress: "7 Ajibola Faminu street"
+}
+
+function destructivelyUpdateEmployeeWithKeyAndValue(employee, key, value) {
+    employee[key] = value;
+    return employee;
+}
+
+function updateEmployeeWithKeyAndValue(employee, key, value) {
+    let newObj = {...employee}
+    newObj[key] = value;
+    return newObj;
+}
+
+function deleteFromEmployeeByKey(employee, key) {
+    let newObj = {...employee}
+    delete newObj[key];
+    return newObj;
+}
+
+function destructivelyDeleteFromEmployeeByKey(employee, key) {
+    delete employee[key];
+    return employee;
+}
